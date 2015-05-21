0214dae1fb84b0197f57619e4426b6cbe2ce6d8a
Egon Willighagen <egon.willighagen@gmail.com>
2015-05-21 19:29:19 +0200

    Close to a unit test: small GPML in, few triples out. Testing using String.contains()

---
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/GpmlConvertorTest.java
@@ -0,0 +1,33 @@
+package org.wikipathways.wp2rdf;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.pathvisio.core.model.ConverterException;
+import org.pathvisio.core.model.Pathway;
+import org.wikipathways.wp2rdf.io.PathwayReader;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class GpmlConvertorTest {
+
+	private String toString(Model model) {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		model.write(output, "TURTLE");
+		return new String(output.toByteArray());
+	}
+	
+	@Test
+	public void testDataNode() throws ConverterException {
+		InputStream input = this.getClass().getClassLoader().getResourceAsStream("gpmlParts/datanode.gpml");
+		Pathway pathway = PathwayReader.readPathway(input);
+		Model model = GpmlConverter.convert(pathway, "WP4", "42");
+		String turtle = toString(model);
+		Assert.assertTrue(turtle.contains("<http://rdf.wikipathways.org/Pathway/WP4_r42/DataNode/b06fe>"));
+		Assert.assertTrue(turtle.contains("gpml:DataNode"));
+		Assert.assertTrue(turtle.contains("gpml:centerX"));
+	}
+	
+}
