946eb93a49bb8319bbd8e74bffd09976a288c9d9
Egon Willighagen <egon.willighagen@gmail.com>
2015-06-19 18:59:20 +0200

    More testing

---
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/WP805Test.java
@@ -0,0 +1,31 @@
+package org.wikipathways.wp2rdf;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pathvisio.core.model.ConverterException;
+
+public class WP805Test extends AbstractConvertorTest {
+
+	@BeforeClass
+	public static void createModel() throws ConverterException {
+		loadModelAsGPMLRDF("WP805_74440.gpml", "WP805", "74440");
+	}
+
+	@Test
+	public void noIdentifierURIs() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("structure/noIdentifierURIs.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertEquals("Data nodes with a 'noIdentifier' URI:\n" + table, 0, table.getRowCount());
+	}
+
+	@Test
+	public void nullPubMedIDs() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("pubs/nullPubMedIDs.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertEquals("PubMed with a null IDs:\n" + table, 0, table.getRowCount());
+	}
+
+}
