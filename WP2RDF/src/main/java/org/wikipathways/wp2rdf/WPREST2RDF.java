6771baca69ecdb8da0095aaf49c79eb32d93a079
Egon Willighagen <egon.willighagen@gmail.com>
2015-06-19 13:11:59 +0200

    Only pathways that are in the Curated or Reactome collections

--- WP2RDF/src/main/java/org/wikipathways/wp2rdf/WPREST2RDF.java
+++ WP2RDF/src/main/java/org/wikipathways/wp2rdf/WPREST2RDF.java
@@ -4,12 +4,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bridgedb.bio.Organism;
 import org.pathvisio.core.model.ConverterException;
 import org.pathvisio.core.model.Pathway;
+import org.pathvisio.wikipathways.webservice.WSCurationTag;
 import org.pathvisio.wikipathways.webservice.WSPathwayInfo;
 import org.wikipathways.client.WikiPathwaysClient;
 import org.wikipathways.wp2rdf.utils.Utils;
@@ -20,45 +23,64 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 public class WPREST2RDF {
 
 	@SuppressWarnings("serial")
+	private static final List<String> INCLUDED_TAGS = new ArrayList<String>() {{
+		add("Curation:AnalysisCollection");
+		add("Curation:Reactome_Approved");
+	}};
+
+	@SuppressWarnings("serial")
+	private static final Map<Organism,String> SPECIES = new HashMap<Organism,String>() {{
+		put(Organism.HomoSapiens, "human");
+	}};
+
 	public static void main(String[] args) throws NumberFormatException, ConverterException, IOException {
 		URL url = new URL("http://webservice.wikipathways.org");
 		WikiPathwaysClient client = new WikiPathwaysClient(url);
 
-		Map<Organism,String> species = new HashMap<Organism,String>() {{
-			put(Organism.ArabidopsisThaliana, "arabidopsisthaliana");
-		}};
-
-		for (Organism organism : species.keySet()) {
+		for (Organism organism : SPECIES.keySet()) {
+			System.out.println("Processing species: " + organism);
 			WSPathwayInfo [] pathways = client.listPathways(organism);
+			System.out.println("  found #pathways: " + pathways.length);
 			for(WSPathwayInfo pwInfo : pathways) {
-				System.out.println(pwInfo.getId() + "\t" + pwInfo.getRevision());
+				System.out.println("  pathway: " + pwInfo.getId() + "\t" + pwInfo.getRevision());
 
-				Model pathwayModel = ModelFactory.createDefaultModel();
-				Utils.setModelPrefix(pathwayModel);
+				// to be converted? only if it has the right tag
+				WSCurationTag[] tags = client.getCurationTags(pwInfo.getId());
+				if (isIncludedTag(tags)) {
+					Model pathwayModel = ModelFactory.createDefaultModel();
+					Utils.setModelPrefix(pathwayModel);
 
-				Pathway p = WikiPathwaysClient.toPathway(client.getPathway(pwInfo.getId(), Integer.parseInt(pwInfo.getRevision())));
+					Pathway p = WikiPathwaysClient.toPathway(client.getPathway(pwInfo.getId(), Integer.parseInt(pwInfo.getRevision())));
 
-				// New conversion of the pathway in GPML vocabulary
-				GpmlConverter.convertGpml(p, pwInfo.getId(), pwInfo.getRevision(), pathwayModel);
+					// New conversion of the pathway in GPML vocabulary
+					GpmlConverter.convertGpml(p, pwInfo.getId(), pwInfo.getRevision(), pathwayModel);
 
-				String folder = "output/gpml/" + species.get(organism) + "/";
-				new File(folder).mkdirs();
-				FileWriter output = new FileWriter(folder + pwInfo.getId() +".ttl");
-				pathwayModel.write(output, "TURTLE");
-				output.close();
+					String folder = "output/gpml/" + SPECIES.get(organism) + "/";
+					new File(folder).mkdirs();
+					FileWriter output = new FileWriter(folder + pwInfo.getId() +".ttl");
+					pathwayModel.write(output, "TURTLE");
+					output.close();
 
-				// New conversion of the pathway in WP vocabulary
-				pathwayModel = ModelFactory.createDefaultModel();
-				Utils.setModelPrefix(pathwayModel);
-				GpmlConverter.convertWp(p, pwInfo.getId(), pwInfo.getRevision(), pathwayModel);
+					// New conversion of the pathway in WP vocabulary
+					pathwayModel = ModelFactory.createDefaultModel();
+					Utils.setModelPrefix(pathwayModel);
+					GpmlConverter.convertWp(p, pwInfo.getId(), pwInfo.getRevision(), pathwayModel);
 
-				folder = "output/wp/" + species.get(organism) + "/";
-				new File(folder).mkdirs();
-				output = new FileWriter(folder + pwInfo.getId() +".ttl");
-				pathwayModel.write(output, "TURTLE");
-				output.close();
+					folder = "output/wp/" + SPECIES.get(organism) + "/";
+					new File(folder).mkdirs();
+					output = new FileWriter(folder + pwInfo.getId() +".ttl");
+					pathwayModel.write(output, "TURTLE");
+					output.close();
+				}
 			}
 		}
+	}
+
+	private static boolean isIncludedTag(WSCurationTag[] tags) {
+		for (WSCurationTag tag : tags) {
+			if (INCLUDED_TAGS.contains(tag.getName())) return true;
+		}
+		return false;
 	}
 
 }
