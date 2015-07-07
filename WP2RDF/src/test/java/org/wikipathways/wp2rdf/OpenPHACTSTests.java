5d0c7e382644c0e87b69e5425ed1d08d931d4523
RyAMiller <rmiller1773@mail.usciences.edu>
2015-07-07 16:23:36 +0200

    added unit tests for rest of the OPS API queries

--- WP2RDF/src/test/java/org/wikipathways/wp2rdf/OpenPHACTSTests.java
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/OpenPHACTSTests.java
@@ -19,4 +19,78 @@ public class OpenPHACTSTests extends AbstractConvertorTest {
 		Assert.assertNotNull(table);
 		Assert.assertNotSame(0, table.getRowCount());
 	}
+	
+	public void pathwayByCompoundCount() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByCompound_Count.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayByCompound() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByCompound.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayByReferenceCount() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByReference_Count.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayByReference() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByReference.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayByTargetCount() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByTarget_Count.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayByTarget() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayByTarget.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayGetReferences() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayGetReferences.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayGetTargets() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayGetTargets.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayInfo() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayInfo.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayListCount() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayList_Count.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayList() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayList.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	public void pathwayOrganisms() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("openphacts/pathwayOrganisms.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertNotSame(0, table.getRowCount());
+	}
+	
 }
