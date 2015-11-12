91acde9822a722a4eb8ef9fab35fc7fb34838bee
Egon Willighagen <egon.willighagen@gmail.com>
2015-11-12 19:26:46 +0100

    Added a test for PublicationReferences that are given in the GPML but not linked to the Pathway or to a DataNode

---
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/WP1533Test.java
@@ -0,0 +1,28 @@
+package org.wikipathways.wp2rdf;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.bridgedb.IDMapperException;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pathvisio.core.model.ConverterException;
+
+public class WP1533Test extends AbstractConvertorTest {
+
+	@BeforeClass
+	public static void createModel() throws ConverterException, FileNotFoundException, ClassNotFoundException, IOException, IDMapperException {
+		loadModelAsWPRDF("WP1533_82707.gpml", "WP1533", "82707");
+	}
+
+	@Test
+	public void hasUnlinkedPubMedReference() throws Exception {
+		String ttl = toString(model);
+		Assert.assertTrue(
+			"PubMed reference 9789062 is not linked to a DataNode or Pathway, but should appear in the RDF",
+			ttl.contains("/9789062")
+		);
+	}
+
+}
