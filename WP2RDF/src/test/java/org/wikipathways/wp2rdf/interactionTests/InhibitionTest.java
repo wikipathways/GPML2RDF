71d994910d93c28ff9f7a89fd9441aa094fad1ce
Egon Willighagen <egon.willighagen@gmail.com>
2019-06-10 21:22:39 +0200

    Tests for the other GPML files

---
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/interactionTests/InhibitionTest.java
@@ -0,0 +1,18 @@
+package org.wikipathways.wp2rdf.interactionTests;
+
+import org.junit.BeforeClass;
+
+/**
+ * Based on test data by Ryan Miller. See <a href="https://github.com/RyAMiller/WikiPathwaysInteractions">this repository</a>.
+ *
+ * @author Egon Willighagen
+ * @author Ryan Miller
+ */
+public class InhibitionTest extends AbstractInteractionTest {
+
+	@BeforeClass
+	public static void createModel() throws Exception {
+		createModel("Inhibition");
+	}
+
+}
