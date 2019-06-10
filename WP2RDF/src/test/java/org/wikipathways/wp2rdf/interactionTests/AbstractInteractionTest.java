4e454f4b1b0a70cc8d7058a07f9935b638f1449a
Egon Willighagen <egon.willighagen@gmail.com>
2019-06-10 21:17:42 +0200

    Rewritten to use the .props files

--- WP2RDF/src/test/java/org/wikipathways/wp2rdf/interactionTests/AbstractInteractionTest.java
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/interactionTests/AbstractInteractionTest.java
@@ -1,7 +1,12 @@
 package org.wikipathways.wp2rdf.interactionTests;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.pathvisio.core.view.MIMShapes;
+import org.wikipathways.wp2rdf.AbstractConvertorTest;
 import org.wikipathways.wp2rdf.AbstractWPConvertorTest;
 import org.wikipathways.wp2rdf.ResourceHelper;
 import org.wikipathways.wp2rdf.SPARQLHelper;
@@ -12,6 +17,19 @@ abstract public class AbstractInteractionTest extends AbstractWPConvertorTest {
 	protected static int expectedDataNodeCount;
 	protected static int expectedInteractionCount;
 	protected static String expectedType;
+	
+	public static void createModel(String gpmlFilePrefix) throws Exception {
+		MIMShapes.registerShapes();
+		loadModelAsWPRDF("interactionTests/" + gpmlFilePrefix + ".gpml", "WP42", "42");
+		InputStream input = AbstractConvertorTest.class.getClassLoader().getResourceAsStream(
+			"interactionTests/" + gpmlFilePrefix + ".props"
+		);
+		Properties props = new Properties();
+		props.load(input);
+		expectedDataNodeCount = Integer.parseInt(props.getProperty("dataNodeCount"));
+		expectedInteractionCount = Integer.parseInt(props.getProperty("interactionCount"));
+		expectedType = props.getProperty("intType").trim();
+	}
 
 	@Test
 	public void testDataNodeCount() throws Exception {
--- WP2RDF/src/test/java/org/wikipathways/wp2rdf/interactionTests/GeneralIntTest.java
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/interactionTests/GeneralIntTest.java
@@ -1,7 +1,6 @@
 package org.wikipathways.wp2rdf.interactionTests;
 
 import org.junit.BeforeClass;
-import org.pathvisio.core.view.MIMShapes;
 
 /**
  * Based on test data by Ryan Miller. See <a href="https://github.com/RyAMiller/WikiPathwaysInteractions">this repository</a>.
@@ -13,11 +12,7 @@ public class GeneralIntTest extends AbstractInteractionTest {
 
 	@BeforeClass
 	public static void createModel() throws Exception {
-		MIMShapes.registerShapes();
-		loadModelAsWPRDF("interactionTests/GeneralInt.gpml", "WP42", "42");
-		expectedDataNodeCount = 2;
-		expectedInteractionCount = 1;
-		expectedType = "http://vocabularies.wikipathways.org/wp#Interaction";
+		createModel("GeneralInt");
 	}
 
 }
