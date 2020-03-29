4e82d0e9614f349bcc3b09052958bf3b84e24402
Egon Willighagen <egon.willighagen@gmail.com>
2020-03-29 19:51:15 +0200

    Finetuned the script

--- WP2RDF/createTurtle.sh
+++ WP2RDF/createTurtle.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 wpid=$1
-match="/${wpid}/,/Tests/{/${wpid}/b;/Tests/b;p}"
+match="/${wpid}/,/Tests/p"
 
-mvn clean test -Dtest=org.wikipathways.wp2rdf.${wpid}Test | sed -n ${match} | grep -v ".bridge" > ${wpid}.ttl
+mvn clean test -Dtest=org.wikipathways.wp2rdf.${wpid}Test \
+  | sed -n ${match} \
+  | grep -v ".bridge" \
+  | grep -v "=== ${wpid}" \
+  | grep -v "Tests run" \
+  > ${wpid}.ttl
