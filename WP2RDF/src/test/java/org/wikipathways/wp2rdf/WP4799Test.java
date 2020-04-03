8c1ed6f2610ed29272d4b5aaf8f9d8ff59e9fdfa
Egon Willighagen <egon.willighagen@gmail.com>
2020-04-03 08:39:27 +0200

    New pathways

---
+++ WP2RDF/src/test/java/org/wikipathways/wp2rdf/WP4799Test.java
@@ -0,0 +1,25 @@
+package org.wikipathways.wp2rdf;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WP4799Test extends AbstractWPConvertorTest {
+
+	@BeforeClass
+	public static void createModel() throws Exception {
+		loadModelAsWPRDF("WP4799.gpml", "WP4799", "109431");
+	}
+
+	@Test
+	public void nullPubMedIDs() throws Exception {
+		String sparql = ResourceHelper.resourceAsString("pubs/nullPubMedIDs.rq");
+		StringMatrix table = SPARQLHelper.sparql(model, sparql);
+		Assert.assertNotNull(table);
+		Assert.assertEquals("PubMed with a null IDs:\n" + table, 0, table.getRowCount());
+		System.out.println("===== WP4799.gpml =====");
+		String ttlContent = toString(model);
+		System.out.println(ttlContent);
+	}
+
+}
